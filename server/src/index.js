2873444e32655d8a86114dea2a095d009ecabbf9
AvigdorFeldman <avigdor.feldman@e.braude.ac.il>
2026-01-20 22:20:52 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,11 @@ import app from './app.js';
 import http from 'http';
 import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [/^http:\/\/localhost:\d+$/, "https://examonitor-t11n.vercel.app/"], // In production, you should restrict this to your frontend's domain for security.
+        origin: ["https://examonitor-t11n.vercel.app"], // In production, you should restrict this to your frontend's domain for security.
         methods: ["GET", "POST"],
         credentials: true
     }
