ffab957f97c4f0325b35065d1325e38c6851c2ef
AvigdorFeldman <avigdor.feldman@e.braude.ac.il>
2026-01-21 22:10:07 +0200

    Delete examonitor-vite/src/handlers/Socket.js

--- examonitor-vite/src/handlers/Socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client';
-
-const socketUrl = import.meta.env.VITE_API_BASE ?? '';
-
-export const socket = io(socketUrl, {
-  withCredentials: true,
-  autoConnect: false,         
-  transports: ['websocket'],
-});
