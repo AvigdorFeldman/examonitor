49b789b06ca9b744540a2878b49d3dd9116a3fbc
AvigdorFeldman <avigdor.feldman@e.braude.ac.il>
2026-01-21 21:51:43 +0200

    Update SocketContext.jsx

--- examonitor-vite/src/components/state/SocketContext.jsx
+++ examonitor-vite/src/components/state/SocketContext.jsx
@@ -1,34 +1,29 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import { socket } from '../../handlers/Socket';
+import { createContext, useContext, useEffect } from 'react';
+import { socket } from '../../handlers/socket';
 
 const SocketContext = createContext(null);
 
 export const useSocket = () => useContext(SocketContext);
 
 export const SocketProvider = ({ children }) => {
-  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    if (!socket.connected) socket.connect();
+    if (!socket.connected) {
+      socket.connect();
+    }
 
     socket.on('connect', () => {
       console.log('Connected', socket.id);
     });
 
-    // Listen for incoming messages
-    socket.on('message', (msg) => {
-      setMessages((prev) => [...prev, msg]); // update state → triggers re-render
-    });
-
     return () => {
       socket.off('connect');
-      socket.off('message');
       socket.disconnect();
     };
   }, []);
 
   return (
-    <SocketContext.Provider value={{ socket, messages }}>
+    <SocketContext.Provider value={socket}>
       {children}
     </SocketContext.Provider>
   );
